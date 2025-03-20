9ee2f9687aef33fdf40b3953bde4b5f741049477
Marcos Pablo <marquin.pablo@gmail.com>
2025-03-19 21:41:35 -0300

    Setting up point and drawable lines

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from drawable_elements import Line, Point
 from window import Window
 
 def main():
+    point1 = Point(100, 100)
+    point2 = Point(500, 100)
+    line = Line(point1, point2)
     win = Window(800, 600)
+    win.draw_line(line)
     win.wait_for_close()
 
 main()
